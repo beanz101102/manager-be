1a2a2c92f99b971ca8ec1a0fe57d4bf14d783fde
Peih Brown <hiep.dao2092000@gmail.com>
2024-12-03 11:50:50 +0700

    handle cors for localhost

--- src/app.ts
+++ src/app.ts
@@ -37,7 +37,11 @@ class App {
     } */
   private setupMiddlewares(): void {
     this.app.use(cors({
-      origin: 'https://app.phatdat.online',
+      origin: [
+        'https://app.phatdat.online',
+        'http://localhost:3000',
+        'http://127.0.0.1:3000'
+      ],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization'],
